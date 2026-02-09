fbfb293a98fe5a443e3424b33a56926529e27573
Sebastian García <estuardosebastian10@gmail.com>
2026-02-09 00:07:06 -0600

    Corrección de errores y completación de Selection Sort

--- src/app/main.java
+++ src/app/main.java
@@ -1,5 +1,60 @@
 package app;
 
-public class main {
+import sortin.*;
+import utils.crearArchivo;
+import utils.lectorArchivo;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        // Se generan y se cargan los datos
+        crearArchivo generador = new crearArchivo();
+        System.out.println("Generando archivo con números aleatorios...");
+        generador.crearArchivos(); 
+        
+        int[] datosOriginales = generador.leerArchivo();
+        System.out.println("Datos cargados. Total: " + datosOriginales.length);
+        System.out.println("-------------------------------------------------");
+
+        lectorArchivo Orden = new lectorArchivo();
+
+        // Gnome Sort
+        int[] datosGnome = datosOriginales.clone(); // .clone(), crea y devuelve una copia del objeto
+        gnomeSort gnome = new gnomeSort();
+        System.out.println("Iniciando Gnome Sort..");
+        gnome.ordenar(datosGnome);
+        Orden.verificarOrden(datosGnome);
+
+        // Merge Sort
+        int[] datosMerge = datosOriginales.clone();
+        mergeSort merge = new mergeSort();
+        System.out.println("\nIniciando Merge Sort...");
+        merge.ordenar(datosMerge);
+        Orden.verificarOrden(datosMerge);
+
+        // Quick Sort
+        int[] datosQuick = datosOriginales.clone();
+        quicksort quick = new quicksort();
+        System.out.println("\nIniciando Quick Sort...");
+        quick.ordenar(datosQuick);
+        Orden.verificarOrden(datosQuick);
+
+        // Radix Sort
+        int[] datosRadix = datosOriginales.clone();
+        radixsort radix = new radixsort();
+        System.out.println("\nIniciando Radix Sort...");
+        radix.ordenar(datosRadix);
+        Orden.verificarOrden(datosRadix);
+        
+        // Selection Sort
+        int[] datosSelection = datosOriginales.clone();
+        selectionSort selection = new selectionSort();
+        System.out.println("\nIniciando Selection Sort...");
+        selection.ordenar(datosSelection);
+        System.out.println("Selection Sort finalizado.");
+        Orden.verificarOrden(datosSelection);
+    }
+
     
 }
--- src/sortin/selectionSort.java
+++ src/sortin/selectionSort.java
@@ -1,5 +1,35 @@
 package sortin;
 
 public class selectionSort {
+
+
+    // Busca repetidamente el elemento mínimo de la parte no ordenada
+    // y lo pone al principio.
+    // Complejidad: O(n^2) en todos los casos aunque esté ordenado
+    public void ordenar(int[] arreglo) {
+        int n = arreglo.length;
+
+        // Uno por uno se mueve el límite del sub-arreglo desordenado
+        for (int i = 0; i < n - 1; i++) {
+            
+            // se encuentra el mínimo en el arreglo
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arreglo[j] < arreglo[min_idx]) {
+                    min_idx = j;
+                }
+            }
+
+            // Se intercambia el elemento mínimo encontrado con el primer elemento
+            // del sub-arreglo desordenado (la posición i)
+            intercambiar(arreglo, min_idx, i);
+        }
+    }
+
     
+    private void intercambiar(int[] arreglo, int a, int b) {
+        int cambio = arreglo[a];
+        arreglo[a] = arreglo[b];
+        arreglo[b] = cambio;
+    }
 }
